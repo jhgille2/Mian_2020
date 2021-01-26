0ceb1ebb9ed79a5e09a755d3bfb73a0a10f5e46d
Jay <Jhgille2@ncsu.edu>
2021-01-26 16:24:47 -0500

    Added a line to the package list to install required packages if they haven't been installed already.

--- packages.R
+++ packages.R
@@ -1,4 +1,24 @@
 ## library() calls go here
+
+# Check if the packages used in the workflow are installed and install them if they are not
+RequiredPackages <- c("conflicted", 
+                      "dotenv", 
+                      "drake", 
+                      "tidyverse",
+                      "readxl",
+                      "here",
+                      "tools",
+                      "janitor",
+                      "skimr",
+                      "openxlsx",
+                      "ggrepel",
+                      "ggplot2",
+                      "cowplot")
+
+new.packages <- RequiredPackages[!(RequiredPackages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# Load libraries
 library(conflicted)
 library(dotenv)
 library(drake)
